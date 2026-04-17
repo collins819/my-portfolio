21d35f7aa9bd81c67ad201b3ed2d6eeebf0a6998
Collins <collinsyewa@gmail.com>
2026-04-17 11:18:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("nav a");
+  const currentPage = window.location.pathname.split("/").pop();
+
+  links.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (
+      linkPage === currentPage ||
+      (currentPage === "" && linkPage === "index.html")
+    ) {
+      link.classList.add("active");
+    }
+  });
+});
